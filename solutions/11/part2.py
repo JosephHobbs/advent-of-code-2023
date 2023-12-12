9d34d4d732e89f5a95591a1d3552eaed585053b8
Joseph Hobbs <joseph.hobbs@gmail.com>
2023-12-11 23:25:58 -0500

    day 11 part 2

--- solutions/11/part2.py
+++ solutions/11/part2.py
@@ -1,6 +1,6 @@
 ################################################################################
 # Advent-of-Code 2023
-# Day 11 Part 01
+# Day 11 Part 02
 ################################################################################
 
 #
@@ -15,14 +15,29 @@ class Galaxy():
         self.coord_y = y
 
 
+class VoidTracker():
+
+    def __init__(self):
+        self.voids_x = []
+        self.voids_y = []
+
+    def add_x(self, col: int):
+        self.voids_x.append(col)
+
+    def add_y(self, row: int):
+        self.voids_y.append(row)
+
+
 class StarMap():
 
     SPACE = '.'
     STAR = '#'
+    VOID_SIZE = 1000000
 
-    def __init__(self, data: list):
+    def __init__(self, data: list, voids: VoidTracker):
         self._data = data
         self.galaxies = []
+        self._voids = voids
         self._find_galaxies()
 
     def _find_galaxies(self):
@@ -31,9 +46,26 @@ class StarMap():
                 if self._data[y][x] == StarMap.STAR:
                     self.galaxies.append(Galaxy(len(self.galaxies), x, y))
 
+    def _expand(self, src: int, dst: int, void_locations):
+        total_voids = 0
+        for void in void_locations:
+            if (src < void and dst > void) or (src > void and dst < void):
+                total_voids += 1
+        
+        return abs(src - dst) + (total_voids * (StarMap.VOID_SIZE - 1))
+
+
     def get_distance(self, source: int, destination: int) -> int:
-        dist_x = abs(self.galaxies[destination].coord_x - self.galaxies[source].coord_x)
-        dist_y = abs(self.galaxies[destination].coord_y - self.galaxies[source].coord_y)
+        dist_x = self._expand(
+            self.galaxies[source].coord_x,
+            self.galaxies[destination].coord_x,
+            self._voids.voids_x)
+
+        dist_y = self._expand(
+            self.galaxies[source].coord_y,
+            self.galaxies[destination].coord_y,
+            self._voids.voids_y)
+
         return (dist_x + dist_y)
 
 
@@ -42,6 +74,7 @@ class Cartographer():
     def generate_star_map(data: list) -> StarMap:
         
         star_map_data = []
+        void_tracker = VoidTracker()
 
         # Figure out which columns contain at least one galaxy and record
         # that information. We need this info to account for galactic
@@ -53,6 +86,10 @@ class Cartographer():
                 if row[idx] == StarMap.STAR:
                     col_has_galaxy[idx] = True
 
+        for i in range(len(col_has_galaxy)):
+            if not col_has_galaxy[i]:
+                void_tracker.add_x(i)
+
         # Run through the inputs again and build our final star map. In this
         # case we're going to transfer the map cell by cell. If we hit a
         # column with no galaxy at all, add another space for expansion. Once
@@ -63,12 +100,10 @@ class Cartographer():
             star_map_data.append([])
             for idx in range(len(row)):
                 star_map_data[-1].append(row[idx])
-                if not col_has_galaxy[idx]:
-                    star_map_data[-1].append(StarMap.SPACE)
             if StarMap.STAR not in star_map_data[-1]:
-                star_map_data.append(star_map_data[-1])
+                void_tracker.add_y(len(star_map_data) - 1)
 
-        return StarMap(star_map_data)
+        return StarMap(star_map_data, void_tracker)
 
 #
 # MAIN
